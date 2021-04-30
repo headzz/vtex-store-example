444b094d6d631d103ed681cd5e8c88fb08d0f9ff
Pedro Muniz <pedro.muniz@qflash.com.br>
2021-04-30 18:52:05 -0300

    adjusting components to fetch data

--- src/modules/Header/components/NavRight/NavRight.tsx
+++ src/modules/Header/components/NavRight/NavRight.tsx
@@ -1,29 +1,55 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Button from 'components/Button'
 import Flexbox from 'components/Flexbox/Flexbox'
 import Input from 'components/Input'
 import Link from 'components/Link'
+
+import { useEffect } from 'react'
+import useFetch from 'utils/hooks/hook'
 import { HeaderRightStyled } from './styled'
 
-const NavRight: React.FC = () => (
-  <HeaderRightStyled>
-    <Flexbox alignContent="right">
-      <Input />
-      <Link
-        href="https://www.google.com"
-        margin
-        background={false}
-        border={false}
-      >
-        Login
-      </Link>
-      <Button
-        icon="cart"
-        round={false}
-        color=""
-        clickHandler={() => console.log('teste')}
-      />
-    </Flexbox>
-  </HeaderRightStyled>
-)
+const NavRight = () => {
+  const toFetch = useFetch()
+  const {
+    state: { availablePokemon, search, filteredPokemon }
+  } = toFetch
+  const {
+    events: { fetchPokemon, fetchSpecificPokemon, searchedPokemon, setSearch }
+  } = toFetch
+
+  useEffect(() => {
+    fetchPokemon()
+  }, [])
+
+  useEffect(() => {
+    searchedPokemon()
+  }, [availablePokemon, search])
+
+  return (
+    <HeaderRightStyled>
+      <Flexbox alignContent="right">
+        <Input
+          setParentValue={setSearch}
+          filtered={filteredPokemon}
+          clickHandler={fetchSpecificPokemon}
+        />
+        <Link
+          href="https://www.google.com"
+          margin
+          background={false}
+          border={false}
+        >
+          Login
+        </Link>
+        <Button
+          icon="cart"
+          round={false}
+          color=""
+          clickHandler={() => console.log('teste')}
+        />
+      </Flexbox>
+    </HeaderRightStyled>
+  )
+}
 
 export default NavRight
